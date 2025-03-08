6767992911df1f00ed9392e45fb791e5b761abc4
OctavioSesana <osesana@frro.utn.edu.ar>
2025-03-08 14:48:56 -0300

    nueva propiedad: mail

--- src/reserva/reserva.controler.ts
+++ src/reserva/reserva.controler.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express'
-import { Reserva } from './reserva.entity.js'
-import { orm } from '../shared/db/orm.js'
+import { Request, Response, NextFunction } from "express";
+import { Reserva } from "./reserva.entity.js";
+import { orm } from "../shared/db/orm.js";
 
-const em = orm.em
+const em = orm.em;
 
 function sanitizedReservaInput(
   req: Request,
@@ -14,81 +14,71 @@ function sanitizedReservaInput(
     horaInicio: req.body.horaInicio,
     horaFin: req.body.horaFin,
     totalReserva: req.body.totalReserva,
-    idCliente: req.body.idCliente,
+    mail_cliente: req.body.mail_cliente,
     idCancha: req.body.idCancha,
     idEmpleado: req.body.idEmpleado,
     idArticulo: req.body.idArticulo,
-  }
+  };
   //more checks here
 
   Object.keys(req.body.sanitizedInput).forEach((key) => {
     if (req.body.sanitizedInput[key] === undefined) {
-      delete req.body.sanitizedInput[key]
+      delete req.body.sanitizedInput[key];
     }
-  })
-  next()
+  });
+  next();
 }
 
 async function findAll(req: Request, res: Response) {
   try {
-    const reservas = await em.find(
-      Reserva,
-      {},
-    )
-    res.status(200).json({ message: 'found all reservas', data: reservas })
+    const reservas = await em.find(Reserva, {});
+    res.status(200).json({ message: "found all reservas", data: reservas });
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 }
 
 async function findOne(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.id)
-    const reserva = await em.findOneOrFail(
-      Reserva,
-      { id },
-    )
-    res.status(200).json({ message: 'found reserva', data: reserva })
+    const id = Number.parseInt(req.params.id);
+    const reserva = await em.findOneOrFail(Reserva, { id });
+    res.status(200).json({ message: "found reserva", data: reserva });
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 }
 
 async function add(req: Request, res: Response) {
   try {
-    const reserva = em.create(Reserva, req.body.sanitizedInput)
-    await em.flush()
-    res.status(201).json({ message: 'reserva created', data: reserva })
+    const reserva = em.create(Reserva, req.body.sanitizedInput);
+    await em.flush();
+    res.status(201).json({ message: "reserva created", data: reserva });
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 }
 
 async function update(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.id)
-    const reservaToUpdate = await em.findOneOrFail(Reserva, { id })
-    em.assign(reservaToUpdate, req.body.sanitizedInput)
-    await em.flush()
-    res
-      .status(200)
-      .json({ message: 'reserva updated', data: reservaToUpdate })
+    const id = Number.parseInt(req.params.id);
+    const reservaToUpdate = await em.findOneOrFail(Reserva, { id });
+    em.assign(reservaToUpdate, req.body.sanitizedInput);
+    await em.flush();
+    res.status(200).json({ message: "reserva updated", data: reservaToUpdate });
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 }
 
 async function remove(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.id)
-    const reserva = em.getReference(Reserva, id)
-    await em.removeAndFlush(reserva)
-    res
-      .status(200)
-      .json({ message: 'reserva deleted' })
+    const id = Number.parseInt(req.params.id);
+    const reserva = em.getReference(Reserva, id);
+    await em.removeAndFlush(reserva);
+    res.status(200).json({ message: "reserva deleted" });
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 }
 
-export { sanitizedReservaInput, findAll, findOne, add, update, remove }
+export { sanitizedReservaInput, findAll, findOne, add, update, remove };
--- src/reserva/reserva.entity.ts
+++ src/reserva/reserva.entity.ts
@@ -1,33 +1,26 @@
-import {
-    Entity,
-    Property,
-    ManyToOne,
-    Rel,
-  } from '@mikro-orm/core'
-  import { BaseEntity } from '../shared/db/baseEntity.entity.js'
-  
-  @Entity()
-  export class Reserva extends BaseEntity {
+import { Entity, Property, ManyToOne, Rel } from "@mikro-orm/core";
+import { BaseEntity } from "../shared/db/baseEntity.entity.js";
 
-    @Property({ nullable: false })
-    fechaReserva!: string
+@Entity()
+export class Reserva extends BaseEntity {
+  @Property({ nullable: false })
+  fechaReserva!: string;
 
-    @Property({ nullable: false })
-    horaInicio!: string
+  @Property({ nullable: false })
+  horaInicio!: string;
 
-    @Property({ nullable: false })
-    horaFin!: string
+  @Property({ nullable: false })
+  horaFin!: string;
 
-    @Property({ nullable: false })
-    totalReserva!: number
+  @Property({ nullable: false })
+  totalReserva!: number;
 
-    @Property({ nullable: false })
-    idCliente!: number
+  @Property({ nullable: false })
+  mail_cliente!: string;
 
-    @Property({ nullable: false })
-    idCancha!: number
+  @Property({ nullable: false })
+  idCancha!: number;
 
-    @Property({ nullable: false })
-    idEmpleado!: number
-
-  }
+  @Property({ nullable: false })
+  idEmpleado!: number;
+}
